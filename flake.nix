4dcd5608c44015873166d42e8d8df0c5495db33f
Valera <valeratrades@gmail.com>
2026-05-05 22:11:06 +0000

    fix: wip: maybe fix python setup

--- flake.nix
+++ flake.nix
@@ -1,87 +1,111 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/b3d51a0365f6695e7dd5cdf3e180604530ed33b4";
-    rust-overlay.url = "github:oxalica/rust-overlay/3a0ebe5d2965692f990cb27e62f501ad35e3deeb";
-    flake-utils.url = "github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b";
-    v_flakes.url = "github:valeratrades/v_flakes/257142a54b071bb8a8b2e031d69e70f416518a5f";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv.url = "github:cachix/devenv/v1.6.1";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+    v_flakes.url = "github:valeratrades/v_flakes?ref=v1.6";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, v_flakes }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-          allowUnfree = true;
-        };
-        rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-          extensions = [ "rust-src" "rust-analyzer" "rust-docs" "rustc-codegen-cranelift-preview" ];
-        });
+  outputs = inputs@{ self, nixpkgs, rust-overlay, flake-parts, devenv, pre-commit-hooks, v_flakes }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        devenv.flakeModule
+      ];
 
-        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-        pname = manifest.name;
-        stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+      systems = nixpkgs.lib.systems.flakeExposed;
 
-        rs = v_flakes.rs { inherit pkgs rust; };
-        github = v_flakes.github {
-          inherit pkgs pname rs;
-          enable = true;
-          lastSupportedVersion = "nightly-2025-10-10";
-          jobs = {
-            errors.replace = [ "rust-tests" ];
-            warnings.replace = [ "rust-doc" "rust-clippy" "rust-machete" "rust-sorted" "tokei" ];
-            other.replace = [ "loc-badge" ];
+      perSystem = { config, self', inputs', system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+            config.allowUnfree = true;
           };
-        };
-        readme = v_flakes.readme-fw {
-          inherit pkgs pname;
-          lastSupportedVersion = "nightly-1.92";
-          rootDir = ./.;
-          licenses = [{ license = v_flakes.files.licenses.blue_oak; }];
-          badges = [ "msrv" "crates_io" "docs_rs" "loc" "ci" ];
-        };
-        combined = v_flakes.utils.combine [ rs github readme ];
-      in
-      {
-        packages =
-          let
+          rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+            extensions = [ "rust-src" "rust-analyzer" "rust-docs" "rustc-codegen-cranelift-preview" ];
+          });
+          pre-commit-check = pre-commit-hooks.lib.${system}.run (v_flakes.files.preCommit { inherit pkgs; });
+          manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+          pname = manifest.name;
+          stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+          python = pkgs.python312;
+
+          rs = v_flakes.rs { inherit pkgs rust; };
+          py = v_flakes.py { inherit pkgs; };
+          github = v_flakes.github {
+            inherit pkgs pname rs py;
+            enable = true;
+            lastSupportedVersion = "nightly-2025-10-10";
+            jobs = {
+              errors.replace = [ "rust-tests" ];
+              warnings.replace = [ "rust-doc" "rust-clippy" "rust-machete" "rust-sorted" "tokei" ];
+              other.replace = [ "loc-badge" ];
+            };
+          };
+          readme = v_flakes.readme-fw {
+            inherit pkgs pname;
+            lastSupportedVersion = "nightly-1.92";
+            rootDir = ./.;
+            licenses = [{ license = v_flakes.files.licenses.blue_oak; }];
+            badges = [ "msrv" "crates_io" "docs_rs" "loc" "ci" ];
+          };
+          combined = v_flakes.utils.combine [ rs py github readme ];
+
+          # Native libs that prebuilt Python wheels (numpy, torch, kokoro deps) dlopen at runtime.
+          pyRuntimeLibs = with pkgs; [
+            stdenv.cc.cc.lib # libstdc++.so.6, libgcc_s, libgomp
+            zlib
+          ];
+
+          rustPlatform = pkgs.makeRustPlatform {
             rustc = rust;
             cargo = rust;
-            rustPlatform = pkgs.makeRustPlatform {
-              inherit rustc cargo stdenv;
-            };
-          in
-          {
-            default = rustPlatform.buildRustPackage rec {
-              inherit pname;
-              version = manifest.version;
+            inherit stdenv;
+          };
+        in
+        {
+          _module.args.pkgs = pkgs;
 
-              nativeBuildInputs = with pkgs; [ pkg-config ];
+          packages.default = rustPlatform.buildRustPackage {
+            inherit pname;
+            version = manifest.version;
 
-              cargoLock.lockFile = ./Cargo.lock;
-              src = pkgs.lib.cleanSource ./.;
-            };
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+
+            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.lib.cleanSource ./.;
           };
 
-        devShells.default =
-          with pkgs;
-          mkShell {
-            inherit stdenv;
-            shellHook = combined.shellHook;
+          devenv.shells.default = {
+            languages.python = {
+              enable = true;
+              package = python;
+              uv = {
+                enable = true;
+                sync.enable = false;
+              };
+            };
+
+            packages = [
+              pkgs.mold
+              pkgs.pkg-config
+              rust
+            ] ++ pyRuntimeLibs ++ pre-commit-check.enabledPackages ++ combined.enabledPackages;
 
             env = {
               RUST_BACKTRACE = 1;
               RUST_LIB_BACKTRACE = 0;
             };
 
-            packages = [
-              mold
-              pkg-config
-              rust
-              uv
-            ] ++ combined.enabledPackages;
+            enterShell =
+              pre-commit-check.shellHook
+              + combined.shellHook
+              + ''
+                export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath pyRuntimeLibs}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+              '';
           };
-      }
-    );
+        };
+    };
 }
